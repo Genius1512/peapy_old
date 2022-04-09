2887beda4f7952ed622117cd1bbda9ace5ee5e64
Genius1512 <79528697+Genius1512@users.noreply.github.com>
2022-04-09 21:02:28 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="peapy",
-    version="1.0.0",
+    version="1.0.2",
     author="Silvan Schmidt",
     description="A python game engine",
     install_requires=["pygame", "keyboard", "mouse", "shapely"],
