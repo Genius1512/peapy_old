1885c48072ea1dc77270d63f5c14cd17ff7983e7
Genius1512 <silvan.genius@gmail.com>
2022-04-11 12:26:19 +0200

    Upload not working

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-import os
 import setuptools
 
 
 setuptools.setup(
     name="peapy",
-    version="1.1.0",
+    version="1.1.15",
     author="Silvan Schmidt",
     description="A python game engine",
-    install_requires=["pygame", "keyboard", "mouse", "shapely"],
+    install_requires=["pygame==2.1.3.dev2", "keyboard", "mouse", "shapely~=1.8.1.post1"],
     packages=["peapy"],
 )
